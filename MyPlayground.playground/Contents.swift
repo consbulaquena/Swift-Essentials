b9b51b263a637754da0ff0503b9eea83cb48a917
Cons Bulaqueña <consbulaquena@gmail.com>
2018-01-26 12:01:18 +0800

    Add playground file for Swift practice
    for loop practice

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var str = "Hello driven, ecouraging programmer"
+
+//"for loop" that iterates 64 times, graph of the sin wave , 0..64 range , i is each value. Whatever written inside is executed sixty times
+
+for i in 0..<64 {
+    var point = sin(Double(i) * 100)
+}
+
+
+
+
+
+
